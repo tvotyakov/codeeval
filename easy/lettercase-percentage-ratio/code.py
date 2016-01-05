e7aba1c9f608d98af0e6065d9039390eec8cd884
Timofey Votyakov <tvotyakov@gmail.com>
2016-01-05 21:52:06 +0500

    "Lettercase percentage ratio" challenge solution using floating operations. The solution uses less memory, runs a little faster, and brought me a little more ranking points than the previous.

--- easy/lettercase-percentage-ratio/code.py
+++ easy/lettercase-percentage-ratio/code.py
@@ -1,40 +1,38 @@
 #!python3
-from decimal import *
-
 def get_lettercase_ratio(in_str):
-    ''' (string) -> tuple of decimals
+    ''' (string) -> tuple of floats
 
     Returns percentage ratios of lowercase and uppercase
     letters in the in_str relative to the length of in_str.
 
     >>> get_lettercase_ratio('')
-    (Decimal('0'), Decimal('0'))
+    (0.0, 0.0)
 
     >>> get_lettercase_ratio('UPPER')
-    (Decimal('0'), Decimal('100'))
+    (0.0, 100.0)
 
     >>> get_lettercase_ratio('lower')
-    (Decimal('100'), Decimal('0'))
+    (100.0, 0.0)
 
     >>> get_lettercase_ratio('UPPERlower')
-    (Decimal('50.0'), Decimal('50.0'))
+    (50.0, 50.0)
 
     >>> get_lettercase_ratio('thisTHIS')
-    (Decimal('50.0'), Decimal('50.0'))
+    (50.0, 50.0)
 
     >>> get_lettercase_ratio('AAbbCCDDEE')
-    (Decimal('20.0'), Decimal('80.0'))
+    (20.0, 80.0)
 
     >>> print('(%.2f, %.2f)' % get_lettercase_ratio('UkJ'))
     (33.33, 66.67)
     '''
     if (len(in_str) == 0):
-        return Decimal(0), Decimal(0)
+        return 0.0, 0.0
 
     lower_count = len([l for l in in_str if l.islower()])
 
-    lower_percent = Decimal(lower_count) / len(in_str) * 100
-    upper_percent = 100 - lower_percent
+    lower_percent = lower_count / len(in_str) * 100.0
+    upper_percent = 100.0 - lower_percent
     return (lower_percent, upper_percent)
 
 def get_lettercase_ratio_str(in_str):
